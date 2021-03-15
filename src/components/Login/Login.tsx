152d9bc651c84b5efae6605175976ba7d18704f7
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-03-15 14:55:27 +0300

    fix: add redirect if authorized

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,10 +2,17 @@ import './Login.scss';
 
 import LoginForm from './LoginForm/LoginForm';
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Login = () => {
+const Login = (props: { authStore: { isAuthorized: boolean; }; }) => {
    
-    return <div className="login-screen">{<LoginForm />}</div>;
+    return <div className="login-screen">{props.authStore.isAuthorized ? <Redirect to="/" /> : <LoginForm />}</div>;
 };
-
-export default Login;
+let mapStateToProps = (state: { authStore: any}) => {
+    return {
+      authStore: state.authStore,
+    };
+  };
+  
+  export default connect(mapStateToProps, {  })(Login);
